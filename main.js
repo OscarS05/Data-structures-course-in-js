0d37043e2087730bfd84ff2de4237800ebe11529
oscar05 <santiagoM0509@hotmail.com>
2024-10-30 18:40:12 -0500

    slice finished

--- main.js
+++ main.js
@@ -40,23 +40,44 @@ class MyArray {
       this.length++;
     }
     shift(){
+      if(this.length === 0){
+        return undefined;
+      }
+      const firstItem = this.data[0];
+
       for (let i = this.length - this.length; i < this.length; i++) {
         this.data[i] = this.data[i + 1];
       }
       delete this.data[this.length - 1];
       this.length--;
+      return firstItem;
+    }
+
+    // Exercises not present in the course: slice(start, end), reverse()
+    slice(start = 0, end = this.length){
+      let newArray = new MyArray;
+
+      if(start < 0){
+        start = this.length + start;
+      }
+      if(end < 0){
+        end = this.length + end;
+      }
+      if(start >= end){
+        return newArray;
+      }
+
+      for (let i = start; i < end; i++) {
+        newArray.push(this.data[i]);
+      }
+      return newArray;
     }
 }
 
 const myArray = new MyArray();
-
 myArray.push('juan');
 myArray.push('oscar');
 myArray.push('roro');
 myArray.push('lala');
-// myArray.shiftIndex(1);
-console.log(myArray);
-myArray.unshift('pirroro');
-console.log(myArray);
-myArray.shift();
+
 console.log(myArray);
